98c61386179c0226dc836c7d72006746af67307f
Yann MARCOU <creativeyann17@gmail.com>
2024-04-29 10:58:03 -0400

    feat: CLIN-2771 logs

--- src/main/java/bio/ferlab/clin/qlinme/Config.java
+++ src/main/java/bio/ferlab/clin/qlinme/Config.java
@@ -28,7 +28,9 @@ public class Config {
 
   public Config() {
     var rootLogger = ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME));
-    rootLogger.setLevel(Env.prod.equals(env) ? Level.ERROR : Env.qa.equals(env) ? Level.DEBUG : Level.INFO);
+    rootLogger.setLevel(Env.prod.equals(env) ? Level.ERROR : Level.INFO);
+    var qlinLogger = ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger("bio.ferlab.clin"));
+    qlinLogger.setLevel(Env.prod.equals(env) ? Level.INFO : Level.DEBUG);
   }
 
   private Optional<String> getEnv(String name) {
